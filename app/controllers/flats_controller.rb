3df5d4572344854b090ce67e50542b21f6fa3da4
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-19 15:43:15 +0900

    airbnb

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -12,17 +12,36 @@ class FlatsController < ApplicationController
   end
 
   def create
-    raise
+    @flat = Flat.new(strong_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def edit
+    @flat = Flat.find(params[:id])
   end
 
   def update
+    @flat = Flat.find(params[:id])
+    if @flat.update(strong_params)
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to flats_path
   end
 
+  private
 
+  def strong_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
 end
