d7e3940af2c2f5e07af5973712156b4a4262b0c9
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-19 15:01:50 +0900

    airbnb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Destroying the database..."
+
+Flat.destroy_all
+
+puts "Adding flats..."
+10.times do
+  flat = Flat.new(
+    name: Faker::FunnyName.name,
+    address: Faker::Address.street_name,
+    description: Faker::Company.catch_phrase,
+    price_per_night: rand(50..100),
+    number_of_guests: rand(1..5)
+  )
+  flat.save!
+  puts "Saved #{flat.name}"
+end
+puts "Done!"
